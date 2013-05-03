93459b706903c90bae913191e1a876bfb16e1652
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-05-03 23:15:36 +0200

    add githubButton

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -19,3 +19,41 @@ bootswatchTheme <- function(theme = "amelia", list, version = "2.3.1") {
     tagList(list, css)
     
 }
+
+#' Github buttons
+#' 
+#' ...
+#' 
+#' @param user github user
+#' @param repo github repo
+#' @param type button type ("watch", "fork", "follow")
+#' @param count show count (TRUE/FALSE)
+#' @param small small size (TRUE/FALSE)
+#' @export
+githubButton <- function(user = "metagraf", repo = "sparkle", type = "watch", count = TRUE, small = TRUE) {
+    
+    if (small) {
+        height <- 20
+        if (count) {
+            width <- switch(type, watch = 110, fork = 95, follow = 165)
+        } else {
+            width <- switch(type, watch = 62, fork = 53, follow = 132)
+        }
+    } else {
+        height <- 30
+        if (count) {
+                width <- switch(type, watch = 170, fork = 147, follow = 255)
+        } else {
+            width <- switch(type, watch = 96, fork = 82, follow = 204)
+        }
+    }
+
+    count_str <- if (count) "true" else "none"
+    size <- if (small) "none" else "large"
+
+    html <- sprintf("<iframe src=\"http://ghbtns.com/github-btn.html?user=%s&repo=%s&type=%s&count=%s&size=%s\" allowtransparency=\"true\" frameborder=\"0\" scrolling=\"0\" width=\"%s\" height=\"%s\"></iframe>", user, repo, type, count_str, size, width, height)
+
+    shiny::HTML(html)
+}
+
+
